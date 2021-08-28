98695d6a583e558b7a4c2fd5aeec284bd356f272
Muhammad Faisal Farhan <gue.fafa00@gmail.com>
2021-08-28 09:57:26 +0700

    Modul Week 2

---
+++ Week2/src/App.tsx
@@ -0,0 +1,118 @@
+import { Redirect, Route } from 'react-router-dom';
+import { IonApp, IonButton, IonCard, IonCardContent, IonCol, IonContent, IonGrid, IonHeader, IonIcon, IonInput, IonItem, IonLabel, IonRouterOutlet, IonRow, IonTitle, IonToolbar } from '@ionic/react';
+import { calculatorOutline, refreshOutline } from 'ionicons/icons';
+import {useRef, useState} from "react";
+import Home from './pages/Home';
+
+/* Core CSS required for Ionic components to work properly */
+import '@ionic/react/css/core.css';
+
+/* Basic CSS for apps built with Ionic */
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
+
+/* Optional CSS utils that can be commented out */
+import '@ionic/react/css/padding.css';
+import '@ionic/react/css/float-elements.css';
+import '@ionic/react/css/text-alignment.css';
+import '@ionic/react/css/text-transformation.css';
+import '@ionic/react/css/flex-utils.css';
+import '@ionic/react/css/display.css';
+
+/* Theme variables */
+import './theme/variables.css';
+
+const App: React.FC = () => {
+  const [calculatedBMI, setCalculatedBMI] = useState<number>();
+  const [resultBMI, setResultBMI] = useState<string>();
+  const heightInputRef = useRef<HTMLIonInputElement>(null);
+  const weightInputRef = useRef<HTMLIonInputElement>(null);
+
+  const calculateBMI = () => {
+    const enteredWeight = weightInputRef.current!.value;
+    const enteredHeight = heightInputRef.current!.value;
+
+    if(!enteredWeight || !enteredHeight) return;
+    
+    const bmi = +enteredWeight / ((+enteredHeight/100) * (+enteredHeight/100));
+
+    if(bmi < 18.5 ){
+      const result = "Kurus"
+      setResultBMI(result);
+    } else if (bmi < 24.9) {
+      const result = "Normal"
+      setResultBMI(result);
+    } else if (bmi < 29.9){
+      const result = "Gemuk"
+      setResultBMI(result);
+    } else {
+      const result = "Obesitas"
+      setResultBMI(result);
+    }
+    setCalculatedBMI(bmi);
+  };
+
+  const resetInput = () => {
+    weightInputRef.current!.value = '';
+    heightInputRef.current!.value = '';
+  };
+
+
+
+  return(
+  <IonApp>
+    <IonHeader>
+      <IonToolbar>
+        <IonTitle>BMI Calculator</IonTitle>
+      </IonToolbar>
+    </IonHeader>
+    <IonContent className="ion-padding">
+      <IonGrid>
+        <IonRow>
+          <IonCol>
+            <IonItem>
+              <IonLabel position="floating">Tinggi Badan (cm)</IonLabel>
+              <IonInput ref={heightInputRef}></IonInput>
+            </IonItem>
+          </IonCol>
+        </IonRow>
+        <IonRow>
+          <IonCol>
+            <IonItem>
+              <IonLabel position="floating">Berat Badan (kg)</IonLabel>
+              <IonInput ref={weightInputRef}></IonInput>
+            </IonItem>
+          </IonCol>
+        </IonRow>
+        <IonRow>
+          <IonCol className="ion-text-left">
+            <IonButton onClick={calculateBMI}>
+              <IonIcon slot="start" icon={calculatorOutline}></IonIcon>
+              Calculate
+            </IonButton>
+          </IonCol>
+          <IonCol className="ion-text-right">
+            <IonButton onClick={resetInput}>
+              <IonIcon slot="start" icon={refreshOutline}></IonIcon>
+              Reset
+            </IonButton>
+          </IonCol>
+        </IonRow>
+        {calculatedBMI && (<IonRow>
+          <IonCol>
+            <IonCard>
+              <IonCardContent className="ion-text-center">
+                <h2>{calculatedBMI}</h2>
+                <h2>{resultBMI}</h2>
+              </IonCardContent>
+            </IonCard>
+          </IonCol>
+        </IonRow>)}
+      </IonGrid>
+    </IonContent>
+  </IonApp>
+  )
+};
+
+export default App;
